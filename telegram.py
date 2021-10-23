6bdbf6df3ed81f7d20b4e193f8d719cf36762b0f
Astrocatling <konstantdarl@gmail.com>
2021-10-23 15:06:46 +0300

    First commit

---
+++ telegram.py
@@ -0,0 +1,296 @@
+import os
+
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, \
+    CallbackQueryHandler, TypeHandler
+
+from openapi_client import openapi
+from datetime import datetime, timedelta
+from statistics import mean
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+
+periods_interval = {
+    'd': ({'days': 1, 'hours': 0}, 'hour'),
+    'w': ({'days': 0, 'hours': 7 * 24}, 'day'),
+    'm': ({'days': 30, 'hours': 0}, 'day'),
+    'y': ({'days': 365, 'hours': 0}, 'day'),
+}
+
+
+class Broker:
+    """docstring"""
+
+    def __init__(self):
+        config = get_config()
+        token = config['tokens']['tinkoff']
+        client = openapi.sandbox_api_client(token)
+        client.sandbox.sandbox_register_post()
+        client.sandbox.sandbox_clear_post()
+        client.sandbox.sandbox_currencies_balance_post(
+            sandbox_set_currency_balance_request={"currency": "USD", "balance": 1000})
+        self.client = client
+
+    def set_candles(self, stock=None, period=None, interval=None):
+        self.candles = self.get_profit_stat(stock, period, interval)
+
+    def get_profit_stat(self, stock, period, interval='day'):
+        candles = self.get_market_candles(stock, self.get_last_date(days=period['days'], hours=period['hours']),
+                                          self.get_last_date(), interval)
+        # Костыль для кейса с выходными
+        if interval == 'hour' and period['days'] == 1:
+            if len(candles) < 24:
+                period['hours'] = 72
+                candles = self.get_market_candles(stock,
+                                                  self.get_last_date(days=period['days'], hours=period['hours']),
+                                                  self.get_last_date(), interval)[-24:]
+        return candles
+
+    def get_market_candles(self, stock, start_date, end_date, interval, save=False):
+        candles = self.client.market.market_candles_get(stock['figi'], start_date, end_date, interval)
+        if save:
+            f = open("Candles_" + stock['figi'] + "_" + stock['name'] + ".json", "a")
+            f.write(str(candles))
+            f.close()
+        return candles.payload.candles
+
+    def get_last_date(self, days=0, hours=0):
+        date = str((datetime.now() - timedelta(days=days, hours=hours)).strftime("%Y-%m-%dT%H:%M:%S.%f")) + '+03:00'
+        return date
+
+    # render
+    def get_dygraphs_data(self):
+        candles_close = [candle.c for candle in self.candles]
+        candles_dates = [candle.time.strftime("%Y-%m-%d %H:%M:%S") for candle in self.candles]
+        avg_line = self.avg_line(candles_close)
+        dygraphs = ''.join(
+            [candles_dates[i] + "," + str(candles_close[i]) + "," + str(avg_line[i]) + "\\n" for i in
+             range(len(candles_close))])
+        return dygraphs
+
+    # Статистика
+    def avg_line(self, _data):
+        data = _data.copy()
+        _max = max(data)
+        _min = min(data)
+        _len = len(data)
+        _step = (_max - _min) / _len
+        return [(_min + _min * 0.115 + _step * i * 0.45) for i in range(_len)]
+
+    def get_best_current_price(self):
+        candles_close = [candle.c for candle in self.candles]
+        avg_line = self.avg_line(candles_close)
+        return avg_line[:-1]
+
+    def get_procent_changes(self):
+        data = [candle.c for candle in self.candles]
+        data_open = data[0]
+        data_close = data[(len(data) - 1)]
+        data_max, data_min = (data_close, data_open) if data_close > data_open else (data_open, data_close)
+        # Разница в процентах между началом и концом периода
+        procent_changes = data_max / data_min * 100 - 100
+        procent_changes = procent_changes if data_close > data_open else -procent_changes
+        procent_changes = "{:.2f}".format(procent_changes)
+        return procent_changes
+
+    def avg_candles_indicators(self, candles):
+        cand_stats = {"c": mean([candle.c for candle in candles]),
+                      "o": mean([candle.o for candle in candles]),
+                      "h": mean([candle.h for candle in candles]),
+                      "l": mean([candle.l for candle in candles])}
+        return cand_stats
+
+    def get_month_avg_profit(self, stock):
+        candles = self.get_profit_stat(stock, {'days': 30, 'hours': 0}, interval='day')
+        return self.avg_candles_indicators(candles)['c']
+
+    # Получение списка акций
+    def get_stock(self, name):
+        name = str(name).lower()
+
+        all_market_stocks = self.get_all_market_stocks().payload.instruments
+
+        stocks = [stock for stock in all_market_stocks if name == stock.name.lower()]
+
+        if len(stocks) == 0:
+            stocks = [stock for stock in all_market_stocks if
+                      name in stock.name.lower() or name in stock.ticker.lower()]
+
+        stocks = sorted(stocks, key=lambda item: item.name.lower().index(name) if name in item.name.lower() else 0)
+
+        if len(stocks) == 0:
+            return False
+        elif len(stocks) == 1:
+            return {"stock": stocks[0]}
+        else:
+            return {"stocks": stocks}
+
+    def get_all_market_stocks(self, save=False):
+        stocks = self.client.market.market_stocks_get()
+        if save:
+            f = open("stocks.json", "a")
+            f.write(str(stocks))
+            f.close()
+        return stocks
+
+    def load_best_stocks(self):
+        f = open('BestStocks.json', 'r')
+        stocks = json.load(f)
+        return stocks
+
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text("Привет, по какой акции ты хочешь получить статистику?")
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Help!')
+
+
+def get_stock(update: Update, context: CallbackContext) -> None:
+    res = get_stock_state(update.message.text)
+    msg = update.message.text.lower()
+    if "избранное" in msg or "избранного" in msg:
+        id = str(update.effective_user.id)
+        f = open(id + ".json", 'r')
+        user = json.load(f)
+        logger.info(user)
+        if "сохранить" in msg or "добавить" in msg:
+            stocks_name = msg.split(" ")
+            stocks_name = list(filter(lambda word: word not in ["избранное", "сохранить", "в", "добавить"], stocks_name))
+            for stock in stocks_name:
+                if stock not in user:
+                    stock = check_stock(stock)
+                    logger.info(stock)
+                    if stock:
+                        user['favorites'].append({'name': stock['name'], 'price': stock['price'], 'best_price': stock['best_price']})
+                        update.message.reply_text("Инструмент " + str(stock) + " добавлен в избранное")
+            f.close()
+            f = open(id + ".json", "w")
+            json.dump(user, f)
+        elif "удалить" in msg or "убрать" in msg:
+            stocks_name = msg.split(" ")
+            stocks_name = list(
+                filter(lambda word: word not in ["избранного", "удалить", "из", "убрать"], stocks_name))
+            for stock in stocks_name:
+                if stock in user:
+                    pass
+                else:
+                    stock = check_stock(stock)
+                    logger.info(stock)
+                    if stock:
+                        favorite = filter(lambda favorite: favorite["name"] == stock, user['favorites'])[0]
+                        user['favorites'].remove(favorite)
+                        update.message.reply_text("Инструмент " + str(stock) + " удалён из избранного")
+            f.close()
+            f = open(id + ".json", "w")
+            json.dump(user, f)
+        else:
+            for stock in user['favorites']:
+                res = get_stock_state(stock["name"])
+                update.message.reply_text(res)
+        f.close()
+    elif "изменения" in msg:
+        id = str(update.effective_user.id)
+        f = open(id + ".json", 'r')
+        user = json.load(f)
+        logger.info(user)
+    else:
+        if type(res) == str:
+            update.message.reply_text(res)
+        else:
+            update.message.reply_text('Уточни что вы имели ввиду:', reply_markup=res)
+
+def check_stock(stock_name):
+    broker = Broker()
+    _stock = broker.get_stock(stock_name)
+    if _stock:
+        if "stock" in _stock:
+            stock = _stock['stock'].to_dict()
+            return stock
+        elif "stocks" in _stock:
+            return False
+    else:
+        return False
+
+
+def get_stock_back(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    query.answer()
+    res = get_stock_state(query.data)
+
+    query.edit_message_text(res)
+
+
+def get_stock_state(stock_name):
+    period = {'days': 365, 'hours': 0}
+    interval = 'day'
+    broker = Broker()
+    _stock = broker.get_stock(stock_name)
+    if _stock:
+        if "stock" in _stock:
+            stock = _stock['stock'].to_dict()
+            broker.set_candles(stock, period, interval)
+            best_price = broker.get_best_current_price()[-1]
+            res_text = stock['name'] + "\n\n" + \
+                       "Current price: " + str(broker.candles[-1].c) + "\n\n" + \
+                       "Best price: " + str(round(best_price, 2)) + "\n\n" + \
+                       "Profit: " + str(round(round(best_price, 2) - broker.candles[-1].c, 2)) + " (" + str(
+                round(((round(best_price, 2) / broker.candles[-1].c) - 1) * 100, 2)) + "%)"
+            return res_text
+        elif "stocks" in _stock:
+            names = [stock.to_dict()['name'] for stock in _stock['stocks']]
+            keyboard = [[InlineKeyboardButton(name[:30], callback_data=name[:30])] for name in names]
+
+            reply_markup = InlineKeyboardMarkup(keyboard)
+
+            return reply_markup
+    else:
+        return "Инструментов с таким названием не найдено"
+
+def track_users(update: Update, context: CallbackContext) -> None:
+    """Store the user id of the incoming update, if any."""
+    logger.info(update.effective_user.id)
+    id = str(update.effective_user.id)
+    if not os.path.isfile("users_data/"+id+".json"):
+        f = open("users_data/"+id+".json", "a")
+        f.write('{"id": '+id+'}')
+        f.close()
+    # if context.user_data.get("name") is None:
+    #     context.user_data["name"] = update.message.text
+
+def get_config():
+    file = open("config.config").read()
+    config = eval(file)
+    return config
+
+def main() -> None:
+    config = get_config()
+    updater = Updater(config['tokens']['telegram'])
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(TypeHandler(Update, track_users), group=-1)
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CallbackQueryHandler(get_stock_back))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, get_stock))
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
